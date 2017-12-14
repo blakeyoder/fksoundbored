7a934801af68445b54d53de1e6c29bec21f65b5c
Blake Yoder <yoderblake@gmail.com>
2017-12-14 09:48:12 -0500

    Retrieve data from S3

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ venv:
 	virtualenv --python=python3 venv && venv/bin/python setup.py develop
 
 run: venv
-	FLASK_APP=fksoundbored FKSOUNDBORED_SETTINGS=../settings.cfg venv/bin/flask run
+	FLASK_APP=fksoundbored FLASK_DEBUG=1 FKSOUNDBORED_SETTINGS=../settings.cfg venv/bin/flask run
 
 test: venv
 	FKSOUNDBORED_SETTINGS=../settings.cfg venv/bin/python -m unittest discover -s tests
--- fksoundbored/views.py
+++ fksoundbored/views.py
@@ -1,9 +1,28 @@
+import os
 from flask import render_template
-
 from fksoundbored import app
+import boto3
 
 
 @app.route('/')
 def index():
-    app.logger.warning('sample message')
-    return render_template('index.html')
+    BUCKET = 'fksoundbored'
+
+    client = boto3.client('s3',
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    all_obj = client.list_objects(Bucket=BUCKET)['Contents']
+
+    data = []
+    for obj in all_obj:
+        meta_data = client.get_object(
+                Bucket=BUCKET,
+                Key=obj['Key']
+                )
+
+        obj_dict = {}
+        obj_dict['name'] = meta_data['Metadata']['name']
+        obj_dict['url'] = "https://s3.amazonaws.com/{}/{}".format(BUCKET, obj['Key'])
+        data.append(obj_dict)
+
+    return render_template('index.html', data=data)
