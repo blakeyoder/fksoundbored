7dc2ed2034b116954d109b49175f777f860b23c2
Blake Yoder <yoderblake@gmail.com>
2017-12-22 20:27:44 -0500

    Setup zappa deploys

--- fksoundbored/views.py
+++ fksoundbored/views.py
@@ -9,8 +9,8 @@ def index():
     BUCKET = 'fksoundbored'
 
     client = boto3.client('s3',
-            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+            aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+            aws_secret_access_key=os.environ.get('AWS_SECRET_KEY'))
     all_obj = client.list_objects(Bucket=BUCKET)['Contents']
 
     data = []
