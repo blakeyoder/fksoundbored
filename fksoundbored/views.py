92e06de10dff3d242ea32f8a5b105d22bf821e55
Blake Yoder <yoderblake@gmail.com>
2017-12-23 10:18:41 -0500

    Add session token to the app because lambda

--- fksoundbored/views.py
+++ fksoundbored/views.py
@@ -7,12 +7,11 @@ import boto3
 @app.route('/')
 def index():
     BUCKET = 'fksoundbored'
-    print('here')
-    print(os.environ)
 
     client = boto3.client('s3',
             aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
-            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'))
+            aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY'),
+            aws_session_token=os.environ.get('AWS_SESSION_TOKEN'))
     all_obj = client.list_objects(Bucket=BUCKET)['Contents']
 
     data = []
