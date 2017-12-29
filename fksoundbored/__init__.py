6000d6b58015cb9c572bab2539d077b1e0bc4003
Blake Yoder <yoderblake@gmail.com>
2017-12-28 20:01:59 -0500

    Add logging formatting back in

--- fksoundbored/__init__.py
+++ fksoundbored/__init__.py
@@ -7,6 +7,15 @@ def create_app(object_name):
 
     app.config.from_object(object_name)
 
+    if not app.debug:
+        import logging
+        from logging.handlers import TimedRotatingFileHandler
+        # https://docs.python.org/3.6/library/logging.handlers.html#timedrotatingfilehandler
+        file_handler = TimedRotatingFileHandler(os.path.join(app.config['LOG_DIR'], 'fksoundbored.log'), 'midnight')
+        file_handler.setLevel(logging.WARNING)
+        file_handler.setFormatter(logging.Formatter('<%(asctime)s> <%(levelname)s> %(message)s'))
+        app.logger.addHandler(file_handler)
+
     return app
 
 app = create_app(os.environ.get('CONFIG_PATH'))
